3fa267fac1a414d38d194dcb3af7c8cdb961d5a6
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2017-12-13 19:35:03 -0600

    GetUsuario

--- BossmandadosAPIService/Controllers/UsuarioController.cs
+++ BossmandadosAPIService/Controllers/UsuarioController.cs
@@ -1,15 +1,31 @@
 ﻿using System.Web.Http;
 using Microsoft.Azure.Mobile.Server.Config;
+using System.Threading.Tasks;
+using BossmandadosAPIService.DataObjects;
+using BossmandadosAPIService.Models;
+using System;
 
 namespace BossmandadosAPIService.Controllers
 {
     [MobileAppController]
     public class UsuarioController : ApiController
     {
-        // GET api/Usuario
-        public string Get()
+        [HttpPost]
+        public async Task<Manboss_usuario> GetUsuario(int UsuarioID)
         {
-            return "Hello from custom controller!";
+            using (BossmandadosAPIContext context = new BossmandadosAPIContext())
+            {
+                try
+                {
+
+                    var query = "SELECT * FROM dbo.manboss_usuarios WHERE Id = " + UsuarioID;
+                    var result = await context.Manboss_usuarios.SqlQuery(query).FirstAsync();
+                    return result;
+
+                }
+                catch (Exception ex) { }
+                return null;
+            }
         }
     }
 }
