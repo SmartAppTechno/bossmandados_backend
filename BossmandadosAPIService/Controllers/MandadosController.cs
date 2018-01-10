7f77491de312b6aad20aef6d504aba7ac03b22b3
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2018-01-10 11:22:21 -0600

    GetMandado(ID)

---
+++ BossmandadosAPIService/Controllers/MandadosController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Http;
+using Microsoft.Azure.Mobile.Server.Config;
+using BossmandadosAPIService.DataObjects;
+using System.Threading.Tasks;
+using BossmandadosAPIService.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BossmandadosAPIService.Controllers
+{
+    [MobileAppController]
+    public class MandadosController : ApiController
+    {
+        [HttpPost]
+        public async Task<Manboss_mandados> Mandados(int MandadoID)
+        {
+            using (BossmandadosAPIContext context = new BossmandadosAPIContext())
+            {
+                try
+                {
+
+                    var query = "SELECT * FROM dbo.manboss_mandados WHERE Id = " + MandadoID;
+                    var result = await context.Manboss_mandados.SqlQuery(query).FirstAsync();
+                    return result;
+
+                }
+                catch (Exception ex) {
+                }
+                return null;
+            }
+        }
+        
+    }
+}
