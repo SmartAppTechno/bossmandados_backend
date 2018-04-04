4a2b2dc61ad424588a51531c909a718f6b780e3d
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2018-04-04 10:05:06 -0500

    Nuevo Mandado

--- BossmandadosAPIService/Controllers/NuevoMandadoController.cs
+++ BossmandadosAPIService/Controllers/NuevoMandadoController.cs
@@ -10,7 +10,7 @@ namespace BossmandadosAPIService.Controllers {
     [MobileAppController]
     public class NuevoMandadoController : ApiController {
         [HttpPost]
-        public async Task<List<Manboss_servicio>> TipoMandado() {
+        public async Task<List<Manboss_servicio>> TipoMandado(string tiposMandado) {
             using (BossmandadosAPIContext context = new BossmandadosAPIContext()) {
                 List<Manboss_servicio> servicios = null;
                 try {
@@ -23,20 +23,41 @@ namespace BossmandadosAPIService.Controllers {
             }
         }
         [HttpPost]
-        public async Task<double> CalcularPrecio(List<Manboss_mandados_ruta> ruta) {
-            
-            double tiempo = 30;
-            double distancia = 20; // cobro.Distancia;
-            double tarifa_dinamica = 1;
-            double costo = (tiempo + distancia) * tarifa_dinamica;
-
-            return costo;
+        public List<string> TiposDePago(string tiposPago) {
+            using (BossmandadosAPIContext context = new BossmandadosAPIContext()) {
+                try {
+                    return new List<string> {
+                        "Efectivo", "Tarjeta"
+                    };
+                }
+                catch {
+                    return null;
+                }
+            }
         }
 
         [HttpPost]
-        public async Task<Manboss_mandados> CrearMandado(List<Manboss_mandados_ruta> ruta, Manboss_cliente cliente) {
-            double costo = await CalcularPrecio(ruta);
-            return null;
+        public async Task<Manboss_mandados> CrearMandado(List<Manboss_mandados_ruta> ruta, Manboss_mandados mandado) {
+            using (BossmandadosAPIContext context = new BossmandadosAPIContext()) {
+                try {
+                    string query = "INSERT INTO manboss_mandados " +
+                        "(estado,cliente,total,fecha,tipo_pago,cuenta_pendiente) " +
+                        "VALUES (" + mandado.Estado + "," + mandado.Cliente + "," + mandado.Total + "," + mandado.Fecha.ToString() +
+                        "," + mandado.Tipo_pago + "," + mandado.Cuenta_pendiente + ")";
+                    int row = await context.Database.ExecuteSqlCommandAsync(query);
+
+                    foreach(Manboss_mandados_ruta r in ruta) {
+                        query = "INSERT INTO Manboss_ruta " +
+                        "(mandado,servicio,latitud,longitud,calle,numero,comentarios,terminado) " +
+                        "VALUES (" + mandado.Id + "," + r.Servicio + "," + r.Latitud + "," + r.Longitud +
+                        "," + r.Calle + "," + r.Numero + "," + r.Comentarios + "," + r.Terminado + ")";
+                        row = await context.Database.ExecuteSqlCommandAsync(query);
+                    }
+                }
+                catch {
+                }
+                return null;
+            }
         }
     }
 }
