f143dca6821943cd2d9102523f1e1d744df02df7
Isaac Méndez <isaac95mendez@gmail.com>
2019-01-29 13:28:46 -0600

    Agregar métodos
    Get Servicios, Insertar Mandado e Insertar Rutas

--- BossmandadosAPIService/Controllers/MandadosController.cs
+++ BossmandadosAPIService/Controllers/MandadosController.cs
@@ -86,6 +86,60 @@ namespace BossmandadosAPIService.Controllers
             }
         }
 
+        [HttpPost]
+        public async Task<Manboss_mandados> CrearMandado(int estado, int cliente, float total, string fecha, int tipo_pago)
+        {
+            using (BossmandadosAPIContext context = new BossmandadosAPIContext())
+            {
+                try
+                {
+                    var query = "INSERT INTO manboss_mandados (estado,cliente,total,fecha,tipo_pago)" +
+                        "VALUES (" + estado + "," + cliente + "," + total + "," + fecha + "," + tipo_pago + ")";
+                    int row = await context.Database.ExecuteSqlCommandAsync(query);
+                }
+                catch
+                {
+                }
+                return null;
+            }
+        }
+
+        [HttpPost]
+        public async Task<Manboss_mandados_ruta> CrearRuta(int mandado, int servicio, float latitud, float longitud,string calle, int numero,string comentarios)
+        {
+            using (BossmandadosAPIContext context = new BossmandadosAPIContext())
+            {
+                try
+                {
+                    var query = "INSERT INTO manboss_mandados_rutas (mandado,servicio,latitud,longitud,calle,numero,comentarios)" +
+                        "VALUES (" + servicio + "," + servicio + "," + latitud + "," + longitud + ",'" + calle + "',"+numero+",'"+comentarios+"')";
+                    int row = await context.Database.ExecuteSqlCommandAsync(query);
+                }
+                catch
+                {
+                }
+                return null;
+            }
+        }
+
+        [HttpPost]
+        public async Task<List<Manboss_servicio>> GetServicios()
+        {
+            using (BossmandadosAPIContext context = new BossmandadosAPIContext())
+            {
+                try
+                {
+                    var query = "SELECT * FROM dbo.manboss_servicios";
+                    var result = await context.Manboss_servicios.SqlQuery(query).ToListAsync();
+                    return result;
+                }
+                catch
+                {
+                }
+                return null;
+            }
+        }
+
 
 
     }
