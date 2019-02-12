a3207b1c53858a5b5324156742399d7df2dad552
carolinadp <caroagsmex@hotmail.com>
2019-02-11 21:30:53 -0600

    Terminado api de google directions
    Se termino la funcion de los polylines de una ruta

--- BossmandadosAPIService/Controllers/GoogleDirectionsController.cs
+++ BossmandadosAPIService/Controllers/GoogleDirectionsController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
 using System.Threading.Tasks;
 using BossmandadosAPIService.DataObjects;
 using GoogleMapsApi;
@@ -11,26 +11,21 @@ using GoogleMapsApi.Entities.Directions.Response;
 using GoogleMapsApi.Entities.Common;
 using BossmandadosAPIService.App_Start;
 using Microsoft.Azure.Mobile.Server.Config;
+using BossmandadosAPIService.Models;
+using System.Web.Http;
 
 namespace BossmandadosAPIService.Controllers
 {
     [MobileAppController]
-    public class GoogleDirectionsController : Controller
+    public class GoogleDirectionsController : ApiController
     {
-
-        // GET: GoogleDirections
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        private DirectionsRequest SetUpRequest(ref List<string> ubicaciones)
+        private DirectionsRequest SetUpRequest(ref List<Manboss_mandados_ruta> ubicaciones)
         {
             if (ubicaciones.Count < 2)
             {
                 return null;
             }
-            string originString = ubicaciones.First<string>(), destinationString = ubicaciones.Last<string>();
+            Manboss_mandados_ruta origin = ubicaciones.First<Manboss_mandados_ruta>(), destination = ubicaciones.Last<Manboss_mandados_ruta>();
             string[] waypoints;
 
             ubicaciones.RemoveAt(0);
@@ -38,7 +33,13 @@ namespace BossmandadosAPIService.Controllers
 
             if (ubicaciones.Count > 0)
             {
-                waypoints = ubicaciones.ToArray();
+                waypoints = new string[ubicaciones.Count];
+                int i = 0;
+                foreach (var m in ubicaciones)
+                {
+                    waypoints[i] = m.Latitud.ToString() + "," + m.Longitud.ToString();
+                    i++;
+                }
             }
             else
             {
@@ -47,46 +48,51 @@ namespace BossmandadosAPIService.Controllers
 
             DirectionsRequest request = new DirectionsRequest()
             {
-                Origin = originString,
-                Destination = destinationString,
-                Waypoints = waypoints,
-                SigningKey = GoogleDirectionsConfig.SIGNING_KEY
+                Origin = origin.Latitud.ToString() + "," + origin.Longitud.ToString(),
+                Destination = destination.Latitud.ToString() + "," + destination.Longitud.ToString(),
+                Waypoints = waypoints, 
+                ApiKey = GoogleDirectionsConfig.SIGNING_KEY
             };
 
             return request;
         }
-        [HttpPost]
-        public async Task<Manboss_polyline_direcciones> GetPolyline(List<string> ubicaciones)
-        {
-            try
-            {
-                DirectionsRequest directionsRequest = SetUpRequest(ref ubicaciones);
 
-                if (directionsRequest == null)
+        // POST: GoogleDirections/GetPolyline
+        [HttpPost]
+        public async Task<string> GetPolyline(int MandadoID)
+        {
+            using (BossmandadosAPIContext context = new BossmandadosAPIContext())
+            {
+                try
+                {
+                    var query = "SELECT * FROM dbo.manboss_mandados_rutas WHERE Mandado = " + MandadoID + " AND Terminado = 0";
+                    var ubicaciones = await context.Manboss_mandados_rutas.SqlQuery(query).ToListAsync();
+                    DirectionsRequest directionsRequest = SetUpRequest(ref ubicaciones);
+
+                    if (directionsRequest == null)
+                    {
+                        return null;
+                    }
+
+                    DirectionsResponse directions = await GoogleMaps.Directions.QueryAsync(directionsRequest);
+
+                    return SetUpResponse(directions);
+                }
+                catch
                 {
                     return null;
                 }
-
-                DirectionsResponse directions = await GoogleMaps.Directions.QueryAsync(directionsRequest);
-
-                return SetUpResponse(directions);
-            }
-            catch (Exception e)
-            {
-                //TODO: logging when request fails
-                return null;
             }
         }
 
-        public Manboss_polyline_direcciones SetUpResponse(DirectionsResponse response)
+        private string SetUpResponse(DirectionsResponse response)
         {
             if (response.Status != DirectionsStatusCodes.OK)
             {
                 throw new Exception("Failed to receive Google Directions response: " + response.StatusStr);
             }
 
-            Manboss_polyline_direcciones polyline = new Manboss_polyline_direcciones();
-            polyline.Serial = response.Routes.First<Route>().OverviewPath.GetRawPointsData();
+            string polyline =  response.Routes.First<Route>().OverviewPath.GetRawPointsData();
 
             return polyline;
         }
